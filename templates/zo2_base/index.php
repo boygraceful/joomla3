b751657e89de5d90c1a612175ebdc25e75d9e16a
Vu Quoc Hiep <vqhiep2010@gmail.com>
2013-06-07 16:58:51 +0700

    add new file again

---
+++ templates/zo2_base/index.php
@@ -0,0 +1,22 @@
+<?php
+defined ('_JEXEC') or die ('resticted aceess');
+//access zo2 framework
+/** @var Zo2Framework $zo2 */
+$zo2 = $this->zo2;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <jdoc:include type="head" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/vendor/font-awesome/css/font-awesome.min.css" type="text/css" />
+</head>
+<body>
+<div class="container">
+<!--    <jdoc:include type="modules" name="top" style="xhtml"/>-->
+    <?php echo $zo2::displayMegaMenu('mainmenu'); ?>
+    <jdoc:include type="component" />
+    <jdoc:include type="modules" name="bottom" />
+</div>
+</body>
+</html>
