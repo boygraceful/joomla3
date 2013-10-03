f0041aca66f68a2e7d7c5443fd170b219e40d11f
Vu Quoc Hiep <vqhiep2010@gmail.com>
2013-10-03 14:09:17 +0700

    update layout builder

--- templates/zo2_blank_template/layouts/header.php
+++
@@ -1,5 +0,0 @@
-<!--[if lt IE 9]><script type="text/javascript" src="http://localhost/dev/joomla31/templates/zo2_blank_template/vendor/html5shiv/html5shiv.js"></script>
-<![endif]--><link rel="stylesheet" href="/dev/joomla31/plugins/system/zo2/assets/vendor/bootstrap/css/bootstrap.min.css" type="text/css" />
-<link rel="stylesheet" href="/dev/joomla31/plugins/system/zo2/assets/vendor/font-awesome/css/font-awesome.min.css" type="text/css" />
-<link rel="stylesheet" href="http://localhost/dev/joomla31/templates/zo2_blank_template/assets/css/megamenu.css" type="text/css" />
-<link rel="stylesheet" href="http://localhost/dev/joomla31/templates/zo2_blank_template/assets/css/style.css" type="text/css" />
