efaaae685d1bb4dcf03baa9be9b8ed4334bdb5c3
Vu Quoc Hiep <vqhiep2010@gmail.com>
2013-10-03 14:00:29 +0700

    Complete ShortCodes

---
+++ templates/zo2_blank_template/layouts/footer.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="/dev/joomla31/plugins/system/zo2/assets/vendor/jquery/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="/dev/joomla31/plugins/system/zo2/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://localhost/dev/joomla31/templates/zo2_blank_template/assets/js/megamenu.js"></script>
