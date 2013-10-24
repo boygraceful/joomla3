7e0c5428c7f399d70df3029d2967d813c711159d
Vu Quoc Hiep <vqhiep2010@gmail.com>
2013-10-24 09:56:14 +0700

    Fix layout & menu

--- plugins/system/zo2/formfields/socialorder.php
+++ plugins/system/zo2/formfields/socialorder.php
@@ -161,12 +161,6 @@ class JFormFieldSocialorder extends JFormFieldHidden
             </fieldset>
         ';
 
-        $options = array(
-            JHtml::_('select.option', '1', JText::_('JYES')),
-            JHtml::_('select.option', '0', JText::_('JNO'))
-        );
-
-        //return $this->radiolist($options, $name ,null, 'value', 'text', $value, $name);
         return $html;
     }
 
@@ -184,50 +178,6 @@ class JFormFieldSocialorder extends JFormFieldHidden
         }
 
         return JHtml::_('select.genericlist', $array, $name . '_position', 'class="inputbox"', 'value', 'text', $active, $name . '_position');
-    }
-
-    function radiolist($data, $name, $attribs = null, $optKey = 'value', $optText = 'text', $selected = null, $idtag = false, $translate = false)
-    {
-        reset($data);
-
-        if (is_array($attribs)) {
-            $attribs = JArrayHelper::toString($attribs);
-        }
-
-        $id_text = $idtag ? $idtag : $name;
-
-        $html = '<fieldset class="radio btn-group">';
-
-        foreach ($data as $obj) {
-            $k = $obj->$optKey;
-            $t = $translate ? JText::_($obj->$optText) : $obj->$optText;
-            $id = (isset($obj->id) ? $obj->id : null);
-
-            $extra = '';
-            $extra .= $id ? ' id="' . $obj->id . '"' : '';
-
-            if (is_array($selected)) {
-                foreach ($selected as $val) {
-                    $k2 = is_object($val) ? $val->$optKey : $val;
-
-                    if ($k == $k2) {
-                        $extra .= ' selected="selected"';
-                        break;
-                    }
-                }
-            } else {
-                $extra .= ((string)$k == (string)$selected ? ' checked="checked"' : '');
-            }
-
-            $html .= "\n\t" . "\n\t" . '<input type="radio" name="' . $name . '" id="' . $id_text . $k . '" value="' . $k . '" ' . $extra . ' '
-                . $attribs . '>';
-            $html .= "\n\t" . '<label for="' . $id_text . $k . '" id="' . $id_text . $k . '-lbl" class="radio">' . $t . '</label>';
-        }
-
-        $html .= '</fieldset>';
-        $html .= "\n";
-
-        return $html;
     }
 
 }
